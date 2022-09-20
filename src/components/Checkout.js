7953715f3f352f9e69f24e15276394ad0896c6e7
k <lingweikong10@gmail.com>
2022-09-20 09:06:06 -0500

    Add style to checkout page.

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -30,37 +30,41 @@ function Checkout(props) {
   return (
     <div>
       <Nav cart={props.cart}></Nav>
+
       <div className="checkout">
-        <form action="">
-          <input type="text" placeholder="First Name" />
-          <input type="text" placeholder="Last Name" required />
-          <input type="email" placeholder="Email" required />
-          <input
-            type="tel"
-            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-            placeholder="Phone"
-          ></input>
-          <input type="text" placeholder="Address" required />
-          <input type="text" placeholder="Credit/Debit Card" />
-          <button>Order</button>
-        </form>
-        <div className="checkout-total-cost">
-          <div className="checkout-cost-item">
-            <p>Items:</p>
-            <p>${itemsTotal}</p>
-          </div>
-          <div className="checkout-cost-item">
-            <p>Tax:</p>
-            <p>${tax}</p>
-          </div>
-          <div className="checkout-cost-item">
-            <p>Shipping:</p>
-            <p>${9.99}</p>
-          </div>
-          <hr />
-          <div className="checkout-cost-item checkout-total">
-            <p>Total:</p>
-            <p>${total}</p>
+        <h1>Checkout</h1>
+        <div className="checkout-content">
+          <form action="">
+            <input type="text" placeholder="First Name" />
+            <input type="text" placeholder="Last Name" required />
+            <input type="email" placeholder="Email" required />
+            <input
+              type="tel"
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              placeholder="Phone"
+            ></input>
+            <input type="text" placeholder="Address" required />
+            <input type="text" placeholder="Credit/Debit Card" />
+            <button>Order</button>
+          </form>
+          <div className="checkout-total-cost">
+            <div className="checkout-cost-item">
+              <p>Items:</p>
+              <p>${itemsTotal}</p>
+            </div>
+            <div className="checkout-cost-item">
+              <p>Tax:</p>
+              <p>${tax}</p>
+            </div>
+            <div className="checkout-cost-item">
+              <p>Shipping:</p>
+              <p>${9.99}</p>
+            </div>
+            <hr />
+            <div className="checkout-cost-item checkout-total">
+              <p>Total:</p>
+              <p>${total}</p>
+            </div>
           </div>
         </div>
       </div>
